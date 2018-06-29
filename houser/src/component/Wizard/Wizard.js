3341703e67b66ecd4b02d51cea289b5deba605d0
Sean Kidwell <seankidwell@Seans-MacBook-Pro.local>
2018-06-29 09:55:07 -0600

    forgot to push a blank app at the start of class

---
+++ houser/src/component/Wizard/Wizard.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Wizard extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
